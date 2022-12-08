a08e9402c4184e397484650c9c157de4c28a35cc
hm18161 <18161@hakodate.kosen-ac.jp>
2022-12-08 10:04:01 +0900

    Bが移動処理を追加

--- game10/BALL.java
+++ game10/BALL.java
@@ -12,8 +12,17 @@ public class BALL extends Actor
      * Act - do whatever the BALL wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    int i = 0;
     public void act() 
     {
         // Add your action code here.
+        while(i==0){
+            int A = 0;
+            int B = 360;
+            int x = A + (int)(Math.random()*((B-A)+1));
+            setRotation(x);
+            i++;
+        }
+        move(5);
     }    
 }
