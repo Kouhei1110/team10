b594938274a4e385471363b87714ca42989149e7
hm18161 <18161@hakodate.kosen-ac.jp>
2023-01-26 10:02:16 +0900

    Bがクリア処理を追加

--- game10/BALL.java
+++ game10/BALL.java
@@ -22,19 +22,91 @@ public class BALL extends Actor
     }
     
     int i = 0;
+    int v;
+    int x = 0;
+    int r = 0;
+    int flag = 0;
+    int left = 0;
+    int right = 0;
     public void act() 
     {
         // Add your action code here.
         GreenfootImage img = new GreenfootImage(img_bkup);
         img.scale( width,height );
         setImage(img);
+        getWorld().showText( ""+right, 50, 400 );
+        getWorld().showText( ""+left, 750, 400 );
         while(i==0){
             int A = 0;
             int B = 360;
-            int x = A + (int)(Math.random()*((B-A)+1));
+            x = A + (int)(Math.random()*((B-A)+1));
+            if(x < 10 && x < 350){
+                x = x + 10;
+            }
+            if(x < 190 && x < 170){
+                x = x + 10;
+            }
             setRotation(x);
+            v = 6;
             i++;
         }
-        move(5);
+        move(v);
+        
+        Actor actor1 = getOneIntersectingObject( PLAYER1.class );
+        if( actor1 != null ){
+            v += 1;
+            x = x - 180;
+            x = -x;
+            int A = 0;
+            int B = 20;
+            r = A + (int)(Math.random()*((B-A)+1));
+            setRotation(x);
+        }       
+        Actor actor2 = getOneIntersectingObject( PLAYER2.class );
+        if( actor2 != null ){
+            v += 1;
+            x = x + 180;
+            x = -x;
+            int A = 0;
+            int B = 20;
+            r = A + (int)(Math.random()*((B-A)+1));
+            setRotation(x);
+        }
+        if( x >= 360 ){
+            x -= 360;
+        }
+        if( x <= -360 ){
+            x += 360;
+        }
+        if((getY() <= 1 || getY() >= 449) && flag == 0){
+            x = -x;
+            setRotation(x);
+            flag++;
+        }
+        if(getY() >= 20 && getY() < 430){
+            flag = 0;
+        }
+        
+        
+        if(getX() <= 5){
+            left++;
+            if(left == 3){
+                getWorld().showText( "PLAYER2 WIN!!!", 400, 150 );
+                Greenfoot.stop();
+            }
+            setLocation(400,225);
+            i = 0;
+            Greenfoot.delay(30);
+        }
+        if(getX() >= 795){
+            right++;
+            if(right == 3){
+                getWorld().showText( "PLAYER1 WIN!!!", 400, 150 );
+                Greenfoot.stop();
+            }
+            setLocation(400,225);
+            i = 0;
+            Greenfoot.delay(30);
+        }
     }    
 }
